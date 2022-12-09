d709257bf075d2cc153264de4f6ad339c8e6e54f
Aditya Bikram Arandhara <adityabikrambill@gmail.com>
2022-12-09 13:00:13 +0530

    Added footer and django tags

--- prediction_app/views.py
+++ prediction_app/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from  django.http import HttpResponse
+from django.template import loader
+from datetime import date
+
+current_date = date.today()
 
 # Create your views here.
 def homePage(request):
-    return render(request, 'home.html')
+    # return render(request, 'home.html')
+    template = loader.get_template('home.html')
+    today_date = {
+        'year':current_date.year,
+        'month':current_date.month,
+    }
+
+    return HttpResponse(template.render(today_date, request))
